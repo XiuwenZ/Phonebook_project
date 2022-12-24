9a16f434a9fcb02bf58346f4305bf321681d8ce4
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2022-12-24 01:09:35 -0500

    Update PhonebookEntry.java

--- PhonebookEntry.java
+++ PhonebookEntry.java
@@ -1,51 +1,41 @@
 class PhonebookEntry {
 
-	public PhonebookEntry(Name name, PhoneNumber phoneNumber){
+	public PhonebookEntry(Name name, ArrayList<NewPhoneNumber> all_numbers){
 		this.name = name;
-		this.phoneNumber = phoneNumber;
+		this.all_numbers = all_numbers;
 	}
 	
 	public Name getName() {
 		return name;
 	}
 	
-	public ArrayList<PhoneNumber> getPhoneNumber() {
-		return all_numbers;
-	}
-	
-	public void call() {
-		if(phoneNumber.isTollFree()) {
-			System.out.println("Dialing (toll-free) " + name + ": " + phoneNumber);
-		}
-		else 
-			System.out.println("Dialing " + name + ": " + phoneNumber);
+	public String getPhoneNumber() {
+		return all_numbers.toString();
 	}
 	
 	public String toString() {
-		return name.toString() + ": " + all_numbers;
+		 return name + "'s phone numbers: " + all_numbers;
 	}
 
-	public boolean equals(PhonebookEntry other){
-		return name.equals(other.name) && phoneNumber.equals(other.phoneNumber);
-	}
 	
 	public static PhonebookEntry read(Scanner scanner){
-		if(!scanner.hasNext()) return null;
-		
-		name = new Name(scanner.next(), scanner.next());
-		int count_number = scanner.nextInt();
-
-		for(int i= 0; i<count_number*2; i++) {
-			phoneNumber = new PhoneNumber(scanner.next());
-			ArrayList<PhoneNumber> all_numbers = new ArrayList<PhoneNumber>();
+		try {
+			if (!scanner.hasNext()) return null;
+		      Name name = Name.read(scanner);
+		      int header = scanner.nextInt();
+		      for(int i=0; i<header; i++){
+		        all_numbers.add(i, NewPhoneNumber.read(scanner));
+		     
+		      }
+			return new PhonebookEntry(name, all_numbers);
+		}catch(Exception e) {
+			System.out.println("exception caught in PhonebookEntry read method" + e.getMessage());
+			return null;
 		}
 		
-		all_numbers.add(phoneNumber);
-		return new PhonebookEntry(name, phoneNumber);
 	}
+	private Name name;
+	private static ArrayList<NewPhoneNumber> all_numbers = new ArrayList<NewPhoneNumber>();
 
-	private static Name name;
-	private static PhoneNumber phoneNumber;
-	private static ArrayList<PhoneNumber> all_numbers ;
 	
 }
