807feae9deb00203039a32d7f2dbedde18f7f9ea
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2022-12-24 01:07:17 -0500

    Update PhonebookApp.java

--- PhonebookApp.java
+++ PhonebookApp.java
@@ -1,5 +1,4 @@
-import java.io.*;
-import java.util.*;
+
 
 public class PhonebookApp{
 public static void main(String[]args)throws IOException{
@@ -13,18 +12,18 @@ public static void main(String[]args)throws IOException{
 			System.out.print("lookup, quit (l/q)? ");
 			String input = keyboard.next();
 			String ln, fn;
-			while(input.charAt(0) !='q'){
+			while(input.charAt(0) !='q'){ 
 				if (input.charAt(0) == 'l'){
 					System.out.print("last name? ");
 					ln = keyboard.next();
 					System.out.print("first name? ");
 					fn = keyboard.next();
 					Name name = new Name(ln, fn);
-					Phonebook phonebook = new Phonebook(scanner);
-					System.out.println(phonebook.toString());
+					Phonebook pb = new Phonebook(scanner);
+					System.out.println(pb.toString());
 					//THE PROGRAM DIDN'T TOUCH THE FOLLOWING LINE
-					if(phonebook.lookup(name)) {
-//						System.out.println(name + " 's phone numbers: " + phonebook.toString());
+					if(pb.lookup(name)) {
+						System.out.println(name + " 's phone numbers: " + pb.toString());
 					}
 					else {
 				          System.out.println("-- Name not found");
@@ -45,3 +44,5 @@ public static void main(String[]args)throws IOException{
 }
 	
 }
+
+
