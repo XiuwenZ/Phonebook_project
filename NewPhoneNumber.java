888b49d429ab8aac96375a0c45275fcb6798bcff
Xiuwen Zhu <zhuxiuwen6@gmail.com>
2022-12-24 01:09:00 -0500

    Create NewPhoneNumber.java

---
+++ NewPhoneNumber.java
@@ -0,0 +1,21 @@
+class NewPhoneNumber extends PhoneNumber{
+	  private String description;
+		
+	  public NewPhoneNumber(String number, String description){
+	    super(number);
+	    this.description=description;
+	  }
+	  @Override
+	  public String toString(){
+		  return this.description +": "+ super.toString();
+	  }
+	  public static NewPhoneNumber read(Scanner scanner){
+	    if(!scanner.hasNext()) return null;
+	    String description = scanner.next();
+	    String number = scanner.next();
+//	    System.out.println(description + ": "+ number);
+	    NewPhoneNumber npn = new NewPhoneNumber(number,description);
+//	    System.out.println("new phone number read in "+ npn );
+	    return npn;
+	  }
+}
